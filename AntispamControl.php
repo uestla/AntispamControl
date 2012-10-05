6a6cd3ec928b3253760cb02075488082765f3e17
Petr Kessler <kesspess@gmail.com>
2012-10-05 13:59:24 +0200

    init commit

---
+++ AntispamControl.php
@@ -0,0 +1,56 @@
+<?php
+
+use Nette\Forms\Form;
+use Nette\Forms\Controls\TextInput;
+
+
+class AntispamControl extends TextInput
+{
+	/**
+	 * @return void
+	 */
+	static function register()
+	{
+		Form::extensionMethod('addAntispam', callback(__CLASS__ . '::addAntispam'));
+	}
+
+
+
+	/**
+	 * @param  Form
+	 * @param  string
+	 * @param  string
+	 * @param  string
+	 * @return AntispamControl
+	 */
+	static function addAntispam(Form $form, $name = 'antispam', $label = 'Vymažte obsah tohoto pole', $message = 'Byl detekován pokus o spam.')
+	{
+		return $form[$name] = new static($label, NULL, NULL, $message);
+	}
+
+
+
+	/**
+	 * @param  string
+	 * @param  int
+	 * @param  int
+	 * @param  string
+	 */
+	function __construct($label = NULL, $cols = NULL, $maxLength = NULL, $msg = NULL)
+	{
+		parent::__construct($label, $cols, $maxLength);
+		$this->setDefaultValue('@');
+		$this->addRule(Form::MAX_LENGTH, $msg, 0);
+	}
+
+
+
+	/**
+	 * @param  string
+	 * @return string
+	 */
+	function sanitize($value)
+	{
+		return $value;
+	}
+}
